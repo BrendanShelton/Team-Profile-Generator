7237dec3167bc1c572ac3479547855e0b240185a
Brendan Shelton <brendan.d.shelton@gmail.com>
2022-10-24 21:41:05 -0400

    deleted node modules

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-//const generateMarkdown = require('./utils/generateMarkdown')
+const generateHTML= require('./src/generateHTML')
 const fs = require('fs');
 const inquirer = require('inquirer');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 
-let team = []
+inquirer.registerPrompt("loop", require("inquirer-loop")(inquirer))
 
-const firstQuestions = [{
+let team = []
+const questions = [{
     type: 'input',
     message: 'Enter the team manager\'s name',
     name: 'name',
@@ -28,19 +29,206 @@ const firstQuestions = [{
     name: 'office',
   },
   {
-    type: 'input',
-    message: 'how many other team members are there?',
-    name: 'teamSize',
+    type: 'loop',
+    message: 'Do you want to add more team members?',
+    name: 'members',
+    questions: [
+        {
+            type: 'checkbox',
+            message: 'What is the next team member\'s role',
+            name: 'role',
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            message: 'Enter the team member\'s name',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'Enter the team member\'s employee ID',
+            name: 'ID',
+        },
+        {
+            type: 'input',
+            message: 'Enter the team member\'s email address',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'What is the team member\'s github username?',
+            name: 'github',
+            when: (answers) => answers.role[0] === "Engineer"
+            /*{
+                if (answers.role === ["Engineer"]) {
+                    return true;
+                }}*/
+        },
+        {
+            type: 'input',
+            message: 'What is the name of the team member\'s school?',
+            name: 'school',
+            when: (answers) => answers.role[0] === "Intern"
+            /*{
+                if (answers.role === ["Intern"]) {
+                    return true;
+                }}*/
+        },
+    ]
   }];
 
-// TODO: Create a function to write README file
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((response) => getTeamMembers(response))
+  }
+
+function getTeamMembers (answers) {
+    console.log(answers)
+    const { name, ID, email, office, members } = answers
+    console.log(members)
+    team[0] = new Manager(name, ID, email, office)
+    //for (let i = 0; 1 < members.length; i++)
+    members.forEach(member => {
+        if (member.role[0] === "Engineer") {
+            const { name, ID, email, github } = member
+            let engineer = new Engineer(name, ID, email, github)
+            team.push(engineer)
+        } else {
+            const { name, ID, email, school } = member
+            let intern = new Intern(name, ID, email, school)
+            team.push(intern)
+        }
+    });
+    console.log(team)
+        
+    console.log(generateHTML(team))
+    
+    
+    
+}
+init();
+/*
+let counter = 0
+const managerQuestions = [{
+    type: 'input',
+    message: 'Enter the team manager\'s name',
+    name: 'name',
+  },
+  {
+    type: 'input',
+    message: 'Enter the team manager\'s employee ID',
+    name: 'ID',
+  },
+  {
+    type: 'input',
+    message: 'Enter the team manager\'s email address',
+    name: 'email',
+  },
+  {
+    type: 'input',
+    message: 'Enter the team manager\'s office number',
+    name: 'office',
+  }];
+
+const nextQuestion = [{
+    type: 'checkbox',
+    message: 'Do you want to add an engineer or and intern or are you finished building your team?',
+    name: 'nextMember',
+    choices: ["Engineer", "Intern", "finished"]
+  }]
+
+const generalQuestions = [{
+    type: 'input',
+    message: 'Enter the team member\'s name',
+    name: 'name',
+  },
+  {
+    type: 'input',
+    message: 'Enter the team member\'s employee ID',
+    name: 'ID',
+  },
+  {
+    type: 'input',
+    message: 'Enter the team member\'s email address',
+    name: 'email',
+  }]
+
+const engineerQuestion = [{
+    type: 'input',
+    message: 'Enter the team member\'s github username',
+    name: 'github',
+}]
+
+const internQuestion = [{
+    type: 'input',
+    message: 'Enter the team member\'s school',
+    name: 'school',
+}]
+
+function init() {
+    inquirer
+    .prompt(managerQuestions.concat(nextQuestion))
+    .then((response) => getTeamMembers(response))
+  }
+
 function getTeamMembers (firstAnswers) {
     console.log(firstAnswers)
-    const { name, ID, email, office, teamSize } = firstAnswers
-    team[0] = new Manager()
+    const { name, ID, email, office, nextMember } = firstAnswers
+    team[0] = new Manager(name, ID, email, office)
+    console.log(team)
+    console.log(nextMember)
+    
+    addMember(nextMember)
+    
     
 }
 
+function addMember(next) {
+    counter++
+    console.log("nextMember function")
+    console.log(next)
+    if (next == "Engineer") {
+        addEngineer()
+    }
+    else if (next == "intern") {
+        addIntern()
+    } else{
+        return "finished"
+    }
+
+}
+
+function addEngineer() {
+    let next
+    inquirer
+    .prompt(generalQuestions.concat(engineerQuestion.concat(nextQuestion)))
+    .then((response) => {consolelog(response)
+        const { name, ID, email, github, nextMember } = response
+        let thisMember = new Engineer(name, ID, email, github)
+        team.push(thisMember)
+        next = nextMember
+    })
+    addMember(next)
+}
+
+function addIntern() {
+    let next
+    inquirer
+    .prompt(generalQuestions.concat(internQuestion.concat(nextQuestion)))
+    .then((response) => {consolelog(response)
+        const { name, ID, email, school, nextMember } = response
+        let thisMember = new Intern(name, ID, email, school)
+        team.push(thisMember)
+        next = nextMember
+    })
+    addMember(next)
+}
+
+
+function consolelog(data) {
+    console.log(data)
+}
 
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, generateMarkdown(data), (err) =>
@@ -48,12 +236,8 @@ function writeToFile(fileName, data) {
   );
 }
 
-// TODO: Create a function to initialize app
-function init() {
-  inquirer
-  .prompt(firstQuestions)
-  .then((response) => getTeamMembers(response))
-}
+
+
 
 // Function call to initialize app
-init();
+init();*/
---
+++ src/generateHTML.js
@@ -0,0 +1,82 @@
+//const Engineer = require("../lib/engineer")
+
+function generateHTML(team) {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <h1>My Team</h1>
+        <main>
+            ${generateMemberCards(team)}
+        </main>
+    </body>
+    </html>`
+}
+
+function generateMemberCards(team) {
+    cards = ""
+    team.forEach (member => {
+        if (member.constructor.name === "Manager") {
+            const { name, ID, email, office } = member
+            card += `<section>
+            <h2>${name}</h2>
+            <h3>Manager</h3>
+            <p>ID: ${ID}</p>
+            <p>Email: ${email}</p>
+            <p>Office number: ${office}</p>
+            </section>`
+        } else if (member.constructor.name === "Engineer") {
+            const { name, ID, email, github } = member
+            card += `<section>
+            <h2>${name}</h2>
+            <h3>Engineer</h3>
+            <p>ID: ${ID}</p>
+            <p>Email: ${email}</p>
+            <p>Github: <a href="https://github.com/${github}">username</a></p>
+            </section>`
+        } else {
+            const { name, ID, email, school } = member
+            card += `<section>
+            <h2>${name}</h2>
+            <h3>Engineer</h3>
+            <p>ID: ${ID}</p>
+            <p>Email: ${email}</p>
+            <p>School: ${school}</p>
+            </section>`
+        }
+        
+    })
+
+    return cards
+}
+/*`<section>
+<h2>name</h2>
+<h3>role</h3>
+<p>ID: </p>
+<p>Email:</p>
+<p>Office number: </p>
+</section>`
+
+`<section>
+<h2></h2>
+<h3></h3>
+<p>ID: </p>
+<p>Email:</p>
+<p>Github: <a href="https://github.com/">username</a></p>
+</section>`
+
+`<section>
+<h2></h2>
+<h3></h3>
+<p>ID: </p>
+<p>Email:</p>
+<p>School: </p>
+</section>`*/
+
+module.exports = generateHTML
